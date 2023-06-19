a7f12a98d66c9fd30b9bfa104b23c58a56d43208
richelbilderbeek <richel@richelbilderbeek.nl>
2023-06-19 10:25:27 +0200

    Add Rackham scripts

--- scripts/build_rackham.sh
+++ scripts/build_rackham.sh
@@ -2,8 +2,7 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 10:00
-#SBATCH --job-name gha_gcc_cpp17
-#SBATCH --output build_rackham.log
+#SBATCH --job-name gha_intel_cpp17
 #SBATCH --qos=short
 #
 # Build the project on Rackham, an UPPMAX computer cluster, see
@@ -17,6 +16,10 @@
 #
 #   sbatch -A uppmax2023-2-25 -M snowy scripts/build_rackham.sh
 #
+#  Or using the convenience script:
+#
+#   ./sbatch_richel.sh
+#
 if [[ ! -z "${CLUSTER}" ]]; then
   echo "Working on a cluster"
 else
@@ -24,5 +27,16 @@ else
   exit 42
 fi
 
-g++ main.cpp -o gha_gcc_cpp17
-./gha_gcc_cpp17
+date
+
+
+# Loading intel/2022a and its dependencies
+module load bioinfo-tools wrf-python/1.3.1 ABINIT/8.10.3 Amber/20 CDO/1.9.5 GOTM/5.3-221-gac7ec88d MUMPS/5.5.0 CDO/1.9.5 PRISMS-PF/2.1.1 PROJ/8.1.0 Siesta/4.1-b4 Singular/4.1.2 deal.II/9.1.1-intel intel/2022a
+# Too old, but easier:
+# module load intel/20.4
+
+# Rackham location (GHA location: source /opt/intel/oneapi/setvars.sh)
+source /sw/comp/intel/oneapi/setvars.sh
+
+icpx main.cpp --verbose -o gha_intel_cpp17
+./gha_intel_cpp17
